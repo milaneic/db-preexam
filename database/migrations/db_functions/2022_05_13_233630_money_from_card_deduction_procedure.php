d7b710d9e1dedba1057beb2da50bc352aec83c37
Milan Eic <milaaneic@gmail.com>
2022-05-14 00:09:24 +0200

    money deduct

---
+++ database/migrations/db_functions/2022_05_13_233630_money_from_card_deduction_procedure.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \DB::unprepared(
+            '
+            DROP PROCEDURE IF EXISTS `money_deduction`;
+            CREATE PROCEDURE `money_deduction`(IN amount double(4,2))
+            BEGIN
+            UPDATE cards SET balance=balance-amount WHERE balance >= amount;
+            UPDATE cards SET status="inactive" WHERE balance <= amount;
+            END;
+            '
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('card_deduction_procedure', function (Blueprint $table) {
+            //
+        });
+    }
+};
